a7fdaf88f99f1bf244083141a889035edf0f2636
Sima-Rached <171807037+Sima-Rached@users.noreply.github.com>
2025-07-20 14:04:24 +0100

    the exercises i have done for now. the rest later.

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,13 @@
 const leapYears = function() {
-
+    const year = arguments[0];
+    
+    if (year % 4 === 0) {
+        if (year % 100 === 0) {
+        return year % 400 === 0;
+        }
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
